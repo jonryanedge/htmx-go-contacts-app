82ae77114a4bc77c37d769851c43ba2f1dadf3e2
Jon Edgeworth <jedgeworth@echofive.net>
2024-07-29 09:56:59 -0400

    +echo handler routes

---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func getContacts(c echo.Context) error            { return nil }
+func getContactsNew(c echo.Context) error         { return nil }
+func postContactsNew(c echo.Context) error        { return nil }
+func getContact(c echo.Context) error             { return nil }
+func getContactEmail(c echo.Context) error        { return nil }
+func getContactEdit(c echo.Context) error         { return nil }
+func postContactEdit(c echo.Context) error        { return nil }
+func deleteContact(c echo.Context) error          { return nil }
+func deleteContacts(c echo.Context) error         { return nil }
+func getContactsArchive(c echo.Context) error     { return nil }
+func postContactsArchive(c echo.Context) error    { return nil }
+func deleteContactsArchive(c echo.Context) error  { return nil }
+func getContactsArchiveFile(c echo.Context) error { return nil }
+func getContactsCount(c echo.Context) error       { return nil }
