1ba26a7a3181a36e80155a3c2fb81e53df636051
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-02 20:58:59 -0400

    print contacts

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"go.scuttlebutt.app/internal/data"
 )
 
-func getContacts(c echo.Context) error            { return nil }
+func getContacts(c echo.Context) error {
+	contacts := data.GetContacts()
+	fmt.Printf("contacts: %s\n", contacts)
+	return nil
+}
 func getContactsNew(c echo.Context) error         { return nil }
 func postContactsNew(c echo.Context) error        { return nil }
 func getContact(c echo.Context) error             { return nil }
