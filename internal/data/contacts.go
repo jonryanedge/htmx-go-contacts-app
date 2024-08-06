e7ff808dbd14c4aece18e85f873558367206fd86
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-05 20:23:10 -0400

    +errors map

--- internal/data/contacts.go
+++ internal/data/contacts.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Contact struct {
-	ID    string `json:"id"`
-	First string `json:"first"`
-	Last  string `json:"last"`
-	Phone string `json:"phone"`
-	Email string `json:"email"`
-	// Errors map[string]string `json:"errors"`
+	ID     int               `json:"id"`
+	First  string            `json:"first"`
+	Last   string            `json:"last"`
+	Phone  string            `json:"phone"`
+	Email  string            `json:"email"`
+	Errors map[string]string `json:"errors"`
 }
 
 type Contacts struct {
