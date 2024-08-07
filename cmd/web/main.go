3813d2edf9e5b4f5ae4bad14e9dec900b26bf9fc
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-07 14:55:14 -0400

    +template registry

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"net/http"
+	"errors"
+	"html/template"
+	"io"
 
 	"github.com/labstack/echo/v4"
 )
@@ -10,12 +12,31 @@ type app struct {
 	debug bool
 }
 
+type TemplateRegistry struct {
+	templates map[string]*template.Template
+}
+
+func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	tmpl, ok := t.templates[name]
+	if !ok {
+		err := errors.New("Template not found ->" + name)
+		return err
+	}
+	return tmpl.ExecuteTemplate(w, "base", data)
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.Redirect(http.StatusMovedPermanently, "/contacts")
-	})
-	e.Static("/static", "static")
+
+	templates := make(map[string]*template.Template)
+	templates["layout"] = template.Must(template.ParseFiles("ui/html/controls.html", "ui/html/layout.html", "ui/html/base.html"))
+	templates["home"] = template.Must(template.ParseFiles("ui/html/home.html", "ui/html/base.html"))
+	e.Renderer = &TemplateRegistry{
+		templates: templates,
+	}
+
+	e.GET("/", getIndex)
+	e.GET("/home", getIndex)
 	e.GET("/contacts", getContacts)
 	e.GET("/contacts/new", getContactsNew)
 	e.POST("/contacts/new", postContactsNew)
@@ -30,5 +51,8 @@ func main() {
 	e.DELETE("/contacts/archive", deleteContactsArchive)
 	e.GET("contacts/archive/file", getContactsArchiveFile)
 	e.GET("contacts/count", getContactsCount)
+
+	e.Static("/static", "static")
+
 	e.Logger.Fatal(e.Start(":3333"))
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,11 +11,21 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func getContacts(c echo.Context) error {
-	contacts := data.GetContacts()
-	// data := fmt.Sprintf("contacts: %s\n", contacts)
-	return c.JSON(http.StatusOK, contacts)
+func getIndex(c echo.Context) error {
+	return c.Render(http.StatusOK, "home", nil)
 }
+
+func redirectIndex(c echo.Context) error {
+	return c.Redirect(http.StatusMovedPermanently, "/contacts")
+}
+
+func getContacts(c echo.Context) error {
+	// contacts := data.GetContacts()
+	// data := fmt.Sprintf("contacts: %s\n", contacts)
+	// return c.JSON(http.StatusOK, contacts)
+	return c.Render(http.StatusOK, "layout", nil)
+}
+
 func getContactsNew(c echo.Context) error  { return nil }
 func postContactsNew(c echo.Context) error { return nil }
 func getContact(c echo.Context) error {
@@ -26,6 +36,7 @@ func getContact(c echo.Context) error {
 	}
 	return c.JSON(http.StatusOK, contact)
 }
+
 func getContactEmail(c echo.Context) error        { return nil }
 func getContactEdit(c echo.Context) error         { return nil }
 func postContactEdit(c echo.Context) error        { return nil }
@@ -35,6 +46,7 @@ func getContactsArchive(c echo.Context) error     { return nil }
 func postContactsArchive(c echo.Context) error    { return nil }
 func deleteContactsArchive(c echo.Context) error  { return nil }
 func getContactsArchiveFile(c echo.Context) error { return nil }
+
 func getContactsCount(c echo.Context) error {
 	count := data.GetContactCount()
 	return c.String(http.StatusOK, strconv.Itoa(count))
