dff2ce0ec3408a3c675647d9fd0ed08c7a323aa2
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-10 15:36:50 -0400

    +app dep inj

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,52 +6,52 @@ import (
 	"net/http"
 	"strconv"
 
-	"go.igmp.app/internal/data"
+	"go.igmp.app/internal/contacts"
 
 	"github.com/labstack/echo/v4"
 )
 
-func getIndex(c echo.Context) error {
+func (app *app) getIndex(c echo.Context) error {
 	return c.Render(http.StatusOK, "home", nil)
 }
 
-func redirectIndex(c echo.Context) error {
+func (app *app) redirectIndex(c echo.Context) error {
 	return c.Redirect(http.StatusMovedPermanently, "/contacts")
 }
 
-func getContacts(c echo.Context) error {
+func (app *app) getContacts(c echo.Context) error {
 	trigger := GetHeaders(c, "HX-Trigger")
 	if trigger == "search" {
 		search := c.QueryParam("q")
 		if search != "" {
-			contacts := data.SearchContacts(search)
+			contacts := contacts.SearchContacts(search)
 			data := map[string]interface{}{
 				"Contacts": contacts.Contacts,
-				"Archiver": data.GetArchiver(),
+				"Archiver": &app.archive,
 				"Query":    search,
 			}
 			return c.Render(http.StatusOK, "partial.rows", data)
 		} else {
-			contacts := data.GetContacts()
+			contacts := contacts.GetContacts()
 			data := map[string]interface{}{
 				"Contacts": contacts.Contacts,
-				"Archiver": data.GetArchiver(),
+				"Archiver": &app.archive,
 				"Query":    search,
 			}
 			return c.Render(http.StatusOK, "partial.rows", data)
 		}
 	}
-	contacts := data.GetContacts()
+	contacts := contacts.GetContacts()
 	data := map[string]interface{}{
 		"Contacts": contacts.Contacts,
-		"Archiver": data.GetArchiver(),
+		"Archiver": &app.archive,
 	}
 	// data := fmt.Sprintf("contacts: %s\n", contacts)
 	// return c.JSON(http.StatusOK, contacts)
 	return c.Render(http.StatusOK, "layout", data)
 }
 
-func getContactsNew(c echo.Context) error {
+func (app *app) getContactsNew(c echo.Context) error {
 	data := map[string]interface{}{
 		"Contact": map[string]string{
 			"First": "",
@@ -63,18 +63,18 @@ func getContactsNew(c echo.Context) error {
 	return c.Render(http.StatusOK, "new", data)
 }
 
-func postContactsNew(c echo.Context) error {
+func (app *app) postContactsNew(c echo.Context) error {
 	input := GetContactData(c)
-	err := data.AddContact(input)
+	err := contacts.AddContact(input)
 	if err != nil {
 		return c.String(http.StatusOK, fmt.Sprintf("error: %s", err))
 	}
 	return c.Redirect(http.StatusSeeOther, "/contacts")
 }
 
-func getContact(c echo.Context) error {
+func (app *app) getContact(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	contact, err := data.GetContact(id)
+	contact, err := contacts.GetContact(id)
 	if err != nil {
 		return c.String(http.StatusOK, fmt.Sprintf("error: %s", err))
 	}
@@ -84,18 +84,18 @@ func getContact(c echo.Context) error {
 	return c.Render(http.StatusOK, "view", data)
 }
 
-func getContactEmail(c echo.Context) error {
+func (app *app) getContactEmail(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	contact, err := data.GetContact(id)
+	contact, err := contacts.GetContact(id)
 	if err != nil {
 		return c.String(http.StatusOK, fmt.Sprintf("error: %s", err))
 	}
 	return c.String(http.StatusOK, fmt.Sprintf("%s", contact.Email))
 }
 
-func getContactEdit(c echo.Context) error {
+func (app *app) getContactEdit(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	contact, err := data.GetContact(id)
+	contact, err := contacts.GetContact(id)
 	if err != nil {
 		return c.String(http.StatusOK, fmt.Sprintf("error: %s", err))
 	}
@@ -105,20 +105,20 @@ func getContactEdit(c echo.Context) error {
 	return c.Render(http.StatusOK, "edit", data)
 }
 
-func postContactEdit(c echo.Context) error {
+func (app *app) postContactEdit(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 	input := GetContactData(c)
-	err := data.UpdateContact(id, input)
+	err := contacts.UpdateContact(id, input)
 	if err != nil {
 		return c.String(http.StatusOK, fmt.Sprintf("error: %s", err))
 	}
 	return c.Redirect(http.StatusSeeOther, fmt.Sprintf("/contacts/%d", id))
 }
 
-func deleteContact(c echo.Context) error {
+func (app *app) deleteContact(c echo.Context) error {
 	trigger := GetHeaders(c, "HX-Trigger")
 	id, _ := strconv.Atoi(c.Param("id"))
-	err := data.DeleteContact(id)
+	err := contacts.DeleteContact(id)
 	if err != nil {
 		return c.String(http.StatusOK, fmt.Sprintf("error: %s", err))
 	}
@@ -128,35 +128,48 @@ func deleteContact(c echo.Context) error {
 	return c.Redirect(http.StatusSeeOther, "/contacts")
 }
 
-func deleteContacts(c echo.Context) error {
+func (app *app) deleteContacts(c echo.Context) error {
 	selections := GetSelectedContacts(c, "selected_contact_ids")
 	for _, sel := range selections {
 		id, _ := strconv.Atoi(sel)
-		data.DeleteContact(id)
+		contacts.DeleteContact(id)
 	}
 	return c.Redirect(http.StatusSeeOther, "/contacts")
 }
 
 // ARCHIVE handlers
-func getContactsArchive(c echo.Context) error {
-	// archiver := data.GetArchiver()
-	return nil
+func (app *app) getContactsArchive(c echo.Context) error {
+	archiver := &app.archive
+	data := map[string]interface{}{
+		"Archiver": archiver,
+	}
+	return c.Render(http.StatusOK, "partial.archive", data)
 }
-func postContactsArchive(c echo.Context) error {
-	// archiver := data.GetArchiver()
-	return nil
+func (app *app) postContactsArchive(c echo.Context) error {
+	archiver := &app.archive
+	archiver.Run()
+	data := map[string]interface{}{
+		"Archiver": archiver,
+	}
+	return c.Render(http.StatusOK, "partial.archive", data)
 }
-func deleteContactsArchive(c echo.Context) error {
-	// archiver := data.GetArchiver()
-	return nil
+func (app *app) deleteContactsArchive(c echo.Context) error {
+	archiver := &app.archive
+	archiver.Reset()
+	data := map[string]interface{}{
+		"Archiver": archiver,
+	}
+	return c.Render(http.StatusOK, "partial.archive", data)
 }
-func getContactsArchiveFile(c echo.Context) error {
-	// archiver := data.GetArchiver()
-	return nil
+func (app *app) getContactsArchiveFile(c echo.Context) error {
+	c.Response().Header().Set("Content-Disposition", "attachment; filename=contacts.json")
+	c.Response().Header().Set("Content-Type", c.Request().Header.Get("Content-Type"))
+	archiver := &app.archive
+	return c.File(archiver.ArchiveFile())
 }
 
 // Feature handlers
-func getContactsCount(c echo.Context) error {
-	count := data.GetContactCount()
+func (app *app) getContactsCount(c echo.Context) error {
+	count := contacts.GetContactCount()
 	return c.String(http.StatusOK, "("+strconv.Itoa(count)+" total contacts)")
 }
