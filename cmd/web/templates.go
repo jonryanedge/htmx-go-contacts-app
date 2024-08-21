83cb91c412b3341702d816e93e652a9eb6ffb58d
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-21 19:21:19 -0400

    +templates cache

---
+++ cmd/web/templates.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"html/template"
+	"io/fs"
+	"path/filepath"
+	"time"
+
+	"go.igmp.app/ui"
+)
+
+type templateData struct {
+	CurrentYear     int
+	Form            any
+	Flash           string
+	IsAuthenticated bool
+	CSRFToken       string
+}
+
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+	return t.UTC().Format("02 Jan 2005 at 15:33")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := fs.Glob(ui.Files, "html/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		patterns := []string{
+			"html/base.html",
+			"html/*.html",
+			page,
+		}
+
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
