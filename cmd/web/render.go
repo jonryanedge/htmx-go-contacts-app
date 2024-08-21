01d87a3a9b295374d9b65cff7de5e59787b63bc4
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-21 19:21:51 -0400

    +http render

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"html/template"
 	"io"
+	"net/http"
 	"strings"
-
-	"github.com/labstack/echo/v4"
 )
 
 type TemplateRegistry struct {
 	templates map[string]*template.Template
 }
 
-func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c *http.Request) error {
 	base := "base"
 	if strings.Contains(name, ".") {
 		parts := strings.Split(name, ".")
@@ -26,4 +26,40 @@ func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c 
 		return err
 	}
 	return tmpl.ExecuteTemplate(w, base, data)
+}
+
+func (app *app) SendString(w http.ResponseWriter, r *http.Request, status int, send string) {
+	buf := new(bytes.Buffer)
+	buf.WriteString(send)
+	w.WriteHeader(status)
+	buf.WriteTo(w)
+}
+
+func (app *app) Render(
+	w http.ResponseWriter, r *http.Request, status int, name string, data interface{}) {
+	base := "base"
+
+	if strings.Contains(name, ".") {
+		parts := strings.Split(name, ".")
+		name = parts[1]
+		base = parts[0]
+	}
+
+	page := name + ".html"
+	ts, ok := app.templates[page]
+	if !ok {
+		err := errors.New("Template not found ->" + page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+	err := ts.ExecuteTemplate(buf, base, data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(status)
+	buf.WriteTo(w)
 }
